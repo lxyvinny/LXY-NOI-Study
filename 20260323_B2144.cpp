b7050be40f617f86af0eef5b86f60fa177735d0d
lxyvinny <lxyvinny@gmail.com>
2026-03-23 21:17:25 +0800

    20260323_LXY

--- 20260323_B2144.cpp
+++ 20260323_B2144.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int m, n;
+int A(int m, int n) {
+  if (!m) {
+    return n + 1;
+  } else if (!n) {
+    return A(m - 1, 1);
+  } else {
+    return A(m - 1, A(m, n - 1));
+  }
+}
 int main() {
+  cin >> m >> n;
+  cout << A(m, n);
   return 0;
 }
--- 20260323_B2147.cpp
+++ 20260323_B2147.cpp
@@ -2,18 +2,12 @@
 using namespace std;
 int x;
 double n, sum;
-void f(int m) {
-  sum += sqrt(n);
-  n--;
-  if (n == 1) {
-    sum += sqrt(n + x);
-    return;
-  }
-  f(n); 
+double f(int m) {
+  return sqrt(f(n));
 }
 int main() {
   cin >> n >> x;
   f(n);
-  cout << fixed << setprecision(2) << sum;
+  cout << fixed << setprecision() <<<< f(n);
   return 0;
 }
