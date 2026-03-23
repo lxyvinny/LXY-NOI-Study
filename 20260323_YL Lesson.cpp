58f3ada75772e6fee6bd012077e2d8b76ca79c1e
lxyvinny <lxyvinny@gmail.com>
2026-03-23 20:45:49 +0800

    _LXY

--- 20260323_YL Lesson.cpp
+++ 20260323_YL Lesson.cpp
@@ -1,19 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int i;
-void Cout(int m) {
-  // if (m > i) {
-  //   return;
-  // }
-  // cout << m << '\n';
-  // Cout(m + 1);
-  cout << m << '\n';
-  if (m < i) {
-    Cout(m + 1);
+int n;
+
+void A(int i) {
+  if (i == 0) {
+    return;
   }
+  A(i - 1);
+  cout << i << '\n';
 }
+
 int main() {
-  cin >> i;
-  Cout(1);
+  cin >> n;
+  A(n);
   return 0;
 }
