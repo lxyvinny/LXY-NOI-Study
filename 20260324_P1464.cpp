0df2e0fa0665fddba028fda07786f96f9d8473fa
lxyvinny <lxyvinny@gmail.com>
2026-03-27 19:22:36 +0800

    20260327_LXY

--- 20260324_P1464.cpp
+++ 20260324_P1464.cpp
@@ -1,16 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a, b, c;
+int a, b, c, memo[25][25][25], ans;
 long long W(int a, int b, int c) {
   if (a <= 0 || b <= 0 || c <= 0) {
     return 1;
-  } else if (a > 20 || b > 20 || c > 20) {
-    return W(20, 20, 20);
-  } else if (a < b && b < c) {
-    return W(a, b, c - 1) + W(a, b - 1, c - 1) - W(a, b - 1, c);
-  } else {
-    return W(a - 1, b, c) + W(a - 1, b - 1, c) + W(a - 1, b, c - 1) - W(a - 1, b - 1, c - 1);
   }
+  if (a > 20 || b > 20 || c > 20) {
+    return W(20, 20, 20);
+  }
+  if (!memo[a][b][c]) {
+    return memo[a][b][c];
+  }
+  if (a < b && b < c) {
+    ans = W(a, b, c - 1) + W(a, b - 1, c - 1) - W(a, b - 1, c);
+  } else {
+    ans = W(a - 1, b, c) + W(a - 1, b - 1, c) + W(a - 1, b, c - 1) - W(a - 1, b - 1, c - 1);
+  }
+  memo[a][b][c] = ans;
+  return ans;
 }
 int main() {
   while (1) {
@@ -19,6 +26,6 @@ int main() {
       break;
     }
     cout << "w(" << a << ", " << b << ", " << c << ") = " << W(a, b, c) << '\n';
+    return 0;
   }
-  return 0;
 }
