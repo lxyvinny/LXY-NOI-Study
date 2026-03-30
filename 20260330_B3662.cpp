120a38e04843b364054e628e0df191c2794430b0
lxyvinny <lxyvinny@gmail.com>
2026-03-30 22:38:10 +0800

    20260330_LXY

--- 20260330_B3662.cpp
+++ 20260330_B3662.cpp
@@ -1,23 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, a[1000][1000], cnt, scs, t[4], zb[];
+int n, m, a[1002][1002], cnt, scs, t[4];
 int main() {
   cin >> n >> m;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
       cin >> a[i][j];
     }
   }
   cin >> scs;
   for (int i = 1; i <= scs; i++) {
-    for (int j = 0; j <= 4; j++) {
-      cin >> t[j];
-    }
-    swap(a[t[0]][t[1]], a[t[2]][t[3]]);
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    swap(a[x1][y1], a[x2][y2]);
   }
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      cnt += a[i][j] >= m && a[i][j] >= n;
+  for (int j = 1; j <= m; j++) {
+    for (int i = 1; i <= n; i++) {
+      cnt += a[i][j] > a[i][j - 1] && a[i][j] > a[i - 1][j] && a[i][j] > a[i][j + 1] && a[i][j] > a[i + 1][j];
+      // if (a[i][j] > a[i][j - 1] && a[i][j] > a[i - 1][j] && a[i][j] > a[i][j + 1] && a[i][j] > a[i + 1][j]) {
+      //   cnt++;
+      // }
+    }
+  }
+  // 这里不会输出的原因是下标错误。
+  cout << cnt << '\n';
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      if (a[i][j] > a[i][j - 1] && a[i][j] > a[i - 1][j] && a[i][j] > a[i][j + 1] && a[i][j] > a[i + 1][j]) {
+        cout << i << ' ' << j << '\n';
+      }
     }
   }
   return 0;
---
+++ 20260330_B3708.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, k, fw1 = 1, fw2 = 9;
+int main() {
+  cin >> n >> k;
+  for (int i = 1; i <= n; i++, fw1 *= 10) {
+  }
+  fw1 /= 10;
+  for (int i = 1; i <= n; i++, fw2 *= 10 + 9) {
+  }
+  fw2 /= 10;
+   
+  return 0;
+}
