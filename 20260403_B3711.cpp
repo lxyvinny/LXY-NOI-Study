7aa6c15d5eb33f36abe1c3a617510029e45c9323
lxyvinny <lxyvinny@gmail.com>
2026-04-04 00:45:47 +0800

    20260403_LXY

--- 20260403_B3711.cpp
+++ 20260403_B3711.cpp
@@ -1,29 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long n, ans;
-string a;
+long long n;
+string a, ans;
 bool Check() {
-  ans = 0;
+  int t = 0;
+  if (a.size() == 1) {
+    t = a[0] - '0';
+  } else {
+    t = (a[a.size() - 1] - '0') + (a[a.size() - 2] - '0') * 10;
+  }
+  if (!(t % 4)) {
+    return 1;
+  }
   for (int l = 0; l < a.size(); l++) {
     for (int r = l; r < a.size(); r++) {
-      for (int num = 0; num < a.size(); num++, ans *= 10) {
+      ans = "";
+      for (int num = 0; num < a.size(); num++) {
         if (num < l || num > r) {
-          ans += a[num] - '0';
+          ans += a[num];
         }
       }
-      ans /= 10;
-      if (ans % 4) {
-        return 1;
+      if (ans.size() > 0) {
+        if (ans.size() == 1) {
+          t = ans[ans.size() - 1] - '0';
+        } else if (ans.size() >= 2) {
+          t = (ans[ans.size() - 1] - '0') + (ans[ans.size() - 2] - '0') * 10;
+        }
+        if (!(t % 4)) {
+          return 1;
+        }
       }
     }
   }
   return 0;
 }
+
 int main() {
-  cin >> n;
-  for (; n--;) {
+  for (cin >> n; n--;) {
     cin >> a;
-    cout << (Check() ? "No" : "Yes") << '\n';
+    cout << (Check() ? "Yes" : "No") << '\n';
   }
   return 0;
 }
