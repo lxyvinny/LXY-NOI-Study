2db5b299369022dc0e0ffd3cdbf81f023e381357
lxyvinny <lxyvinny@gmail.com>
2026-04-09 23:11:32 +0800

    20260409_LXY

--- 20260409_LXY_B3765.cpp
+++ 20260409_LXY_B3765.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, a[1001], cnt;  // a[i] 表示 i 可以到哪些网页。
-bool b[1001];         // 标记是否访问过。
+int n, cnt;    // a[i] 表示 i 可以到哪些网页。
+bool b[1001];  // 标记是否访问过。
+vector<int> a[1001];
 int main() {
   cin >> n;
+  // 读入图
   for (int i = 1; i <= n; i++) {
     int t;
     cin >> t;
@@ -15,3 +17,9 @@ int main() {
   }
   return 0;
 }
+
+
+// 读入图
+// 第0步：在1号网页
+// 第1步：从1出发
+// 第2步：从第1步到达的所有点继续走
